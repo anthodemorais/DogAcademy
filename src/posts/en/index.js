23f0251f87b24482fa5876bc78394f14a9b63033
Anthony <anthonyd-m@live.com>
2021-04-15 22:02:14 +0200

    added new articles

--- src/posts/en/index.js
+++ src/posts/en/index.js
@@ -1,8 +1,10 @@
 import bathingADog from './bathing-a-dog-how-and-why'
 import howToTeachToSit from './how-to-teach-a-dog-to-sit'
 import houseTrainADog from './how-to-house-train-a-dog'
+import dogDentalCare from './dog-dental-care-how-to-prevent-dog-dental-health-issues';
 
 const posts = [
+  dogDentalCare,
   houseTrainADog,
   howToTeachToSit,
   bathingADog,
--- src/posts/fr/index.js
+++ src/posts/fr/index.js
@@ -1,8 +1,10 @@
 import bathingADog from './laver-son-chien-comment-et-pourquoi'
 import howToTeachToSit from './comment-apprendre-a-son-chien-a-s-asseoir'
 import houseTrainADog from './comment-apprendre-la-propreté-a-son-chien'
+import dogDentalCare from './prendre-soin-des-dents-des-chiens'
 
 const posts = [
+  dogDentalCare,
   houseTrainADog,
   howToTeachToSit,
   bathingADog,
