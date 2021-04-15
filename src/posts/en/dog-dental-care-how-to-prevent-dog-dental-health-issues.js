673ed68852a4ad90acc29b2252b0bbc5b1b3950f
Anthony <anthonyd-m@live.com>
2021-04-15 21:57:01 +0200

    dog dental care article and responsive fix on tags

---
+++ src/posts/en/dog-dental-care-how-to-prevent-dog-dental-health-issues.js
@@ -0,0 +1,94 @@
+import image from '../images/dog-dental-care.jpg'
+
+const text = `Do you consider yourself a good dog owner? Is your dog not only the happiest pup on the block, but also the healthiest? 
+
+There are nearly 44 million U.S. households that own approximately 74 million dogs. Although many of these owners treat their dog more like a family member and less like an animal, most are unaware of **one of the biggest health risks for their dog**. 
+
+Home dental care is to be given more emphasis nowadays because of the fact that the dental diseases are emerging in the case of pet animals like dogs to a greater extent. Though you are giving home dental care, if you suspect on the extension of the dental diseases, then **immediately approach the veterinarian** for intervention.
+
+Some people don't realize that dental hygiene is as important for dogs as it is for human beings. Just like in people, dogs' teeth can gather **plaque** after eating. When plaque builds up and hardens it becomes a coarse brown substance called tartar. As tartar accumulates it can work its way under the gums and cause painful infections and gum disease. This goes on in the mouths of dogs just like it does in people. You brush your teeth every day, probably three times. What does your dog do?
+
+### Dogs' Most Frequent Health Problems are Dental Diseases
+
+Periodontal disease is the **most frequently diagnosed** health problem in pets. In fact, more than **80 percent** of dogs have it by the time they are 4 years old.
+
+Periodontal disease begins when bacteria and food debris build up along the dog's gum line. Plaque is created and, soon after, tartar forms on the teeth. Eventually the gums swell, and pockets form that can trap bacteria and lead to more serious problems. 
+
+"Doggy breath, loose teeth, bleeding gums, mouth pain and even infections in the heart, liver and kidney are signs of advanced periodontal disease," says Jan Bellows, DVM and owner of *Hometown Animal Hospital and Dental Clinic* in Weston, Florida. "To avoid these health problems, owners must take steps to care for their dog's teeth-much like they care for their own."
+
+Only one in five owners have ever attempted to brush their pet's teeth and only **2 percent brush with enough frequency** to maintain proper oral health. 
+
+In addition to brushing, Dr. Bellows uses ORAVET, a plaque-prevention gel that stops disease-causing bacteria from attaching to the teeth. It is the only sealant available on the market.
+
+After an in-clinic cleaning, the sealant is applied to the dog's teeth, creating an invisible barrier that reduces bacterial plaque adhesion. Owners then apply the odorless, tasteless homecare gel to the dog's teeth once a week. It typically takes less than a minute to apply. 
+
+"As a veterinarian and as a pet owner, this gel gives me a valuable tool in preventing oral disease in dogs," says Dr. Bellows. "It's easy to use and is a perfect complement to tooth brushing, dental diets and dental chews."
+
+### Teeth Brushing for Doggies
+
+Teeth brushes are available for use with care in case of dogs and one has to be careful during the usage of these brushes in dogs. Teeth brushes are to be used with **special kinds of pastes** recommended by the veterinarians for home use in case of dogs.
+
+Veterinarians recommend that dog owners brush their dog's teeth **at least twice a week** to keep the buildup of tartar at a minimum. Most pet supply stores carry specially designed toothbrushes and toothpaste just for dogs. Remember that a dog's sense of taste and smell is far more acute than that of a human and the zesty, tingly, mint taste of toothpastes for people will be extremely awful to a dog. Try brushing Rover's teeth with Crest just once and it will likely be the last time he lets you anywhere near him with a toothbrush. **Use the specially designed doggie toothpaste.**
+
+The plaque materials are intermittently to be removed at home to avoid any occurrence of the periodontal diseases, which are more common among the dogs.  Dental wipes are available in the pet shops and they may be used carefully in the home.  This helps to remove more plaque and the tartar like materials that are loosely attached. 
+
+### Dental Chew
+
+Some people don't have the time or patience to brush their dogs' teeth on a regular basis. If you're one of these, you'll want to care for Chopper's choppers in another way. A dog's natural tendency to chew is a **built-in dental care** mechanism. Dog biscuits break into small chunks when chewed and rub against the teeth, providing a cleaning service. There's no substitute for brushing your dog's teeth, but if you can't do that, **make sure he gets some sort of crunchy dog biscuit on a regular basis.**
+
+Try to provide bone materials without very sharp points to the dogs and they may love to chew them and then swallow the bitten products. Such activities help them to go for the development of strong teeth structures in a natural manner. 
+
+Mind that the breeds of dogs like Pekingese, etc. are more prone for the development of teeth diseases because the teeth are closely crowned in the oral cavity be to the small size of the these dogs. Hence, these dogs need to be checked up for the excessive plaque formation in the home itself. 
+
+Recreational raw beef bones are wonderful materials to keep the teeth structures of your dog clean and free from formation of plaque with build up of bacterial organisms. 
+
+Specially prepared food materials are available in the pet shops to remove the tartar and the plaque materials from the teeth structures. **Dry dog food and toys** that are specially made to add strength to the teeth structures are often used at home for better teeth cleaning.
+
+### Mouth Diseases in Dogs
+
+Preventing dental disease is an important feature, which is to be paid more emphasis by the dog lovers or dog owners. Dental disease is given priority in the health schedule of the dog nowadays because of the association of the dental diseases with systemic diseases in the dogs.
+
+Dogs that do not receive proper dental care and do not have access to crunchy teeth cleaning foods run the risk of several types of mouth disease. These can be as mild as gingivitis (a gum disease that results in swollen, inflamed gums) and as serious as a bacterial infection that can spread through the dog's bloodstream **causing damage to vital organs**. You owe it to yourself and your dog to take care of his teeth.
+
+Antibiotics need to be given in the initial stage of teeth infection itself and if not, this may cause specific infections and the organisms may spread to the other nearby regions like the oral mucous membrane and pharyngeal region etc. 
+
+More acidic or alkaline food materials need not be given to the dogs to avoid the possible teeth damage.  Antibiotics need to be given in the initial stage of teeth infection itself and if not, this may cause specific infections and the organisms may spread to the other nearby regions like the oral mucous membrane and pharyngeal region etc.  More acidic or alkaline food materials need not be given to the dogs to avoid the possible teeth damage.  **If the dog is not given at its young age some bony material to bite on, the dog may develop some dental diseases later.** The teething action often causes the animal to go for biting in an indiscriminate manner. Hence, the animal has to be given some biting materials to avoid the occurrence of dental diseases. 
+
+If there is an evidence of bleeding from the oral region, the dog needs to be examined thoroughly for any dental abnormality.  Mainly the puppies or some times, the adult dogs also may have teeth injuries. They need to be **attended immediately** as a preventive step. If not, the animal may end up in secondary bacterial infections.
+
+Hunting dogs need **additional care** associated with the dental structures and such care is needed to avoid the future dental problems in such dogs.  Brushes are available to provide better dental care to dogs. However, one has to allot more time and should have patience to use such brushes in case of dogs. This may lead into further problems. Centers for disease control and prevention are trying to put up guidelines to prevent the dental diseases in dogs in many nations. However, the oral examination needs to be carried out frequently in dogs and such activities help to rule out the emerging problems pertaining to the dentine structures in the beginning itself.
+
+### Doggie Dentistry
+
+Dental services are available for dogs, just like they are for people. A dog's teeth can be filled, capped, and extracted if necessary, just like a human's. The best course of action, however, is to avoid the need for such services by properly caring for your dog's teeth. If you can avoid unnecessary pain and discomfort for your furry friend, you should do so. Preventative doggie dental care can save you money as well. Doggie dental procedures can be **quite costly**.
+
+### Conclusion
+
+As explained throughout this article, dental diseases are the one of most frequently diagnosed health problems for dogs. If you're not caring about your dog's dental health, you should at least be regularly giving your dog something to chew to prevent dental diseases. That's what veterinarians strongly recommend and you should consider it especially if you don't want to brush your dog's teeth everyday (which I totally understand). It is worth it to spend time on your dog's health as it's a great way to have your dog around for many years.
+
+Here's what you should keep in mind :
+- Dental diseases are the **most frequent health issues** so it's a good idea to care about your dog's teeth
+- Brushing your dog's teeth is recommended by veterinarians but if you don't have time to do it or you don't want to, at least **give him food or toys to chew**
+- Your dog can't talk so to see if he has health issues, you have to **observe him** to see if he has any symptoms. If so, see if you can do something. If you can't do anything or if your dog is really feeling bad, take him to the veterinarian as soon as you can.
+`
+
+const name = 'Dog Dental Care - How to prevent dog dental health issues'
+const slug = 'dog-dental-care-how-to-prevent-dog-dental-health-issues'
+const exerpt = 'More than 80 percent of dogs have dental diseases by the time they are 4 years old. Here, you will learn the basics of dog dental care to prevent these diseases'
+const minRead = 10
+const content = text
+const metaKeywords = 'dog dental care,dog dental health,dog health,dog dental hygiene,dog teeth health,dog dental diseases'
+const tags = ['dog-health']
+
+const post = {
+  name,
+  slug,
+  exerpt,
+  image,
+  minRead,
+  content,
+  metaKeywords,
+  tags
+}
+
+export default post
---
+++ src/posts/fr/prendre-soin-des-dents-des-chiens.js
@@ -0,0 +1,94 @@
+import image from '../images/dog-dental-care.jpg'
+
+const text = `Te considères-tu comme un bon propriétaire de chien ? Ton chien est-il non seulement le chiot le plus heureux du quartier, mais aussi le plus sain ? 
+
+Il y a près de 44 millions de foyers américains qui possèdent environ 74 millions de chiens. Bien que beaucoup de ces propriétaires traitent leur chien comme un membre de la famille et non comme un animal, la plupart d'entre eux ne sont pas conscients de l'un des plus grands risques pour la santé de leur chien. 
+
+Les soins dentaires à domicile doivent être privilégiés de nos jours, car les maladies dentaires sont de plus en plus fréquentes chez les animaux de compagnie comme les chiens. Bien que tu prodigues des soins dentaires à chez toi, si tu soupçonnes l'apparition de maladies dentaires, **contacte immédiatement le vétérinaire** pour qu'il intervienne.
+
+Certaines personnes ne réalisent pas que l'hygiène dentaire est aussi importante pour les chiens que pour les êtres humains. Tout comme chez les humains, les dents des chiens peuvent accumuler de la plaque dentaire après avoir mangé. Lorsque la plaque s'accumule et durcit, elle devient une substance brune grossière appelée tartre. En s'accumulant, le tartre peut se frayer un chemin sous les gencives et provoquer des infections douloureuses et des maladies des gencives. Ce phénomène se produit dans la bouche des chiens tout comme chez les humains. Tu te brosses les dents tous les jours, probablement trois fois. Combien de fois le fait ton chien ?
+
+### Les problèmes de santé les plus fréquents chez les chiens sont des maladies dentaires
+
+Les maladies parodontales sont le problème de santé **le plus fréquemment diagnostiqué chez les animaux de compagnie**. En fait, plus de **80% des chiens en souffrent avant l'âge de 4 ans**.
+
+La maladie parodontale commence lorsque des bactéries et des débris alimentaires s'accumulent le long de la ligne gingivale du chien. De la plaque dentaire se forme et, peu après, du tartre se forme sur les dents. Les gencives finissent par gonfler et des poches se forment, qui peuvent retenir les bactéries et entraîner des problèmes plus graves. 
+
+"Une haleine de chien, des dents qui se déchaussent, des gencives qui saignent, des douleurs buccales et même des infections du cœur, du foie et des reins sont des signes de maladie parodontale avancée", explique Jan Bellows, DVM et propriétaire de *Hometown Animal Hospital and Dental Clinic* à Weston, en Floride. "Pour éviter ces problèmes de santé, les propriétaires doivent prendre des mesures pour prendre soin des dents de leur chien - tout comme ils prennent soin des leurs."
+
+Seul **un propriétaire sur cinq** a déjà essayé de brosser les dents de son animal et seulement **2% le font avec une fréquence suffisante** pour maintenir une bonne santé bucco-dentaire. 
+
+En plus du brossage, le Dr Bellows utilise *ORAVET*, un gel de prévention de la plaque dentaire qui empêche les bactéries pathogènes de se fixer sur les dents. Il s'agit du seul produit d'étanchéité disponible sur le marché.
+
+Après un nettoyage en clinique, le scellant est appliqué sur les dents du chien, créant une barrière invisible qui réduit l'adhésion de la plaque bactérienne. Les propriétaires appliquent ensuite le gel de soins à domicile, inodore et sans goût, sur les dents du chien une fois par semaine. L'application prend généralement moins d'une minute. 
+
+"En tant que vétérinaire et propriétaire d'animaux, ce gel constitue un outil précieux pour prévenir les maladies bucco-dentaires chez les chiens", déclare le Dr Bellows. "Il est facile à utiliser et constitue un complément parfait au brossage des dents, aux régimes alimentaires dentaires et aux produits à mâcher dentaires."
+
+### Brossage des dents pour les toutous
+
+Les brosses à dents sont disponibles pour être utilisées avec précaution dans le cas des chiens et il faut être **prudent** lors de l'utilisation de ces brosses chez les chiens. Les brosses à dents doivent être utilisées avec des dentifrices spéciaux recommandés par les vétérinaires pour un usage domestique chez les chiens.
+
+Les vétérinaires recommandent aux propriétaires de chiens de brosser les dents de leur animal au moins **deux fois par semaine** pour limiter l'accumulation de tartre. La plupart des animaleries proposent des brosses à dents et des dentifrices spécialement conçus pour les chiens. N'oublie pas que les sens du goût et de l'odorat d'un chien sont beaucoup plus aiguisés que ceux d'un humain et que le goût piquant et mentholé des dentifrices destinés aux humains sera extrêmement désagréable pour un chien. Essaye de brosser les dents de Rover avec ton dentifrice une seule fois et ce sera probablement la dernière fois qu'il te laissera t'approcher de lui avec une brosse à dents. **Utilise le dentifrice spécialement conçu pour les chiens.**
+
+La plaque dentaire doit être éliminée **par intermittence** à la maison pour éviter l'apparition de maladies parodontales, qui sont plus fréquentes chez les chiens. Des lingettes dentaires sont disponibles dans les animaleries et peuvent être utilisées avec précaution à la maison.  Elles permettent d'éliminer la plaque dentaire et les matières ressemblant à du tartre qui se sont détachées. 
+
+### Mâchoires dentaires
+
+Certaines personnes n'ont **pas le temps ou la patience** de brosser régulièrement les dents de leur chien. Si tu fais partie de ces personnes, tu devras prendre soin des dents de ton chien d'une autre manière. **La tendance naturelle d'un chien à mâcher est un mécanisme de soin dentaire intégré**. Les biscuits pour chiens se brisent en petits morceaux lorsqu'ils sont mâchés et frottent contre les dents, offrant ainsi un service de nettoyage. Rien ne remplace le brossage des dents de ton chien, mais si tu ne peux pas le faire, veille à ce qu'il **reçoive régulièrement une sorte de biscuit croquant pour chien**.
+
+Essaye de fournir aux chiens des matériaux osseux sans pointes très acérées. Ils aimeront peut-être les mâcher et avaler ensuite les produits mordus. De telles activités les aident à développer des structures dentaires solides de manière naturelle. 
+
+N'oublie pas que les races de chiens comme le pékinois, etc., sont plus sujettes au développement de maladies dentaires, car les dents sont étroitement couronnées dans la cavité buccale en raison de la petite taille de ces chiens. Ces chiens doivent donc faire l'objet d'un contrôle de la formation excessive de plaque dentaire à la maison. 
+
+Les os de bœuf crus à usage récréatif sont des matériaux merveilleux pour garder les structures dentaires de ton chien propres et exemptes de la formation de plaque et de l'accumulation d'organismes bactériens. 
+
+Des aliments spécialement préparés sont disponibles dans les animaleries pour éliminer le tartre et la plaque dentaire. Les aliments secs pour chiens et les jouets spécialement conçus pour renforcer les structures dentaires sont souvent utilisés à la maison pour un meilleur nettoyage des dents.
+
+### Maladies buccales chez le chien
+
+La prévention des maladies dentaires est un aspect important auquel les amateurs ou les propriétaires de chiens doivent accorder plus d'importance. De nos jours, les maladies dentaires sont prioritaires dans le programme de santé du chien, car elles sont associées à des maladies systémiques chez les chiens.
+
+Les chiens qui ne reçoivent pas de soins dentaires appropriés et qui n'ont pas accès à des aliments croquants pour nettoyer les dents courent le **risque de contracter plusieurs types de maladies buccales**. Celles-ci peuvent être aussi bénignes que la gingivite (une maladie des gencives qui entraîne un gonflement et une inflammation des gencives) et aussi graves qu'une infection bactérienne qui peut se propager dans la circulation sanguine du chien et **endommager les organes vitaux**. Tu te dois à toi-même et à ton chien de prendre soin de ses dents.
+
+Les antibiotiques doivent être administrés au stade initial de l'infection des dents. Sinon, cela peut provoquer des infections spécifiques et les organismes peuvent se propager à d'autres régions voisines comme la muqueuse buccale et la région pharyngée, etc. 
+
+Il faut éviter de donner aux chiens des aliments plus acides ou plus alcalins pour éviter d'éventuelles lésions dentaires.  Les antibiotiques doivent être administrés au stade initial de l'infection des dents. Sinon, cela peut provoquer des infections spécifiques et les organismes peuvent se propager à d'autres régions voisines comme la muqueuse buccale et la région pharyngée, etc.  Les aliments plus acides ou alcalins ne doivent pas être donnés aux chiens pour éviter d'éventuelles lésions dentaires.  **Si le chien ne reçoit pas à son jeune âge un matériau osseux à mordre, il risque de développer des maladies dentaires plus tard.** La poussée dentaire incite souvent l'animal à mordre sans discernement. Il faut donc donner à l'animal des matériaux à mordre pour éviter l'apparition de maladies dentaires.
+
+S'il y a des signes de saignement dans la région buccale, le chien **doit être examiné** minutieusement pour détecter toute anomalie dentaire.  Les chiots principalement, ou parfois les chiens adultes, peuvent également présenter des lésions dentaires. Ils doivent être soignés immédiatement à titre préventif. Sinon, l'animal risque de souffrir d'infections bactériennes secondaires.
+
+Les chiens de chasse ont besoin de **soins supplémentaires** associés aux structures dentaires et ces soins sont nécessaires pour éviter les futurs problèmes dentaires chez ces chiens.  Des brosses sont disponibles pour fournir de meilleurs soins dentaires aux chiens. Cependant, il faut consacrer plus de temps et de patience à l'utilisation de ces brosses chez les chiens. Cela peut entraîner d'autres problèmes. Les centres de contrôle et de prévention des maladies tentent de mettre en place des directives pour prévenir les maladies dentaires chez les chiens dans de nombreux pays. Cependant, l'examen buccal doit être effectué fréquemment chez les chiens et ces activités permettent d'écarter les problèmes émergents liés aux structures dentaires dès le début.
+
+### La dentisterie pour chiens
+
+Des services dentaires sont disponibles pour les chiens, tout comme pour les humains. Les dents d'un chien peuvent être plombées, obturées et extraites si nécessaire, tout comme celles d'un humain. Toutefois, la meilleure solution consiste à éviter de recourir à ces services en prenant soin des dents de ton chien. Si tu peux éviter à ton ami à fourrure une douleur et un inconfort inutiles, tu devrais le faire. Les soins dentaires préventifs pour chiens peuvent également vous faire économiser de l'argent. Les procédures dentaires pour chiens **peuvent être assez coûteuses.**
+
+### Conclusion
+
+Comme nous l'avons expliqué dans cet article, les maladies dentaires sont l'un des problèmes de santé **les plus fréquemment diagnostiqués chez les chiens**. Tu devrais au minimum essayer de lui donner régulièrement quelque chose à mâcher surtout quand il est jeune pour prévenir les maladies dentaires. C'est ce que les **vétérinaires recommandent** vivement et tu devrais l'envisager, surtout si tu ne veux pas ou ne peut pas brosser les dents de ton chien tous les jours (ce que je comprends parfaitement). Ça vaut la peine de consacrer du temps à la santé de ton chien car c'est un excellent moyen de l'avoir à tes côtés pendant de nombreuses années.
+
+Voici ce que tu dois garder à l'esprit :
+- Les maladies dentaires sont les problèmes de santé **les plus fréquents**, il est donc important de prendre soin des dents de ton chien.
+- Le brossage des dents de ton chien est recommandé par les vétérinaires, mais si tu n'as pas le temps de le faire ou si tu ne veux pas le faire, **donne-lui au moins de la nourriture ou des jouets à mâcher**.
+- Ton chien ne peut pas parler, donc pour savoir s'il a des problèmes de santé, tu dois **l'observer** pour voir s'il a des symptômes. Si c'est le cas, fais des recherches et vois si tu peux faire quelque chose. Si tu ne peux rien faire ou si ton chien se sent vraiment mal, **emmène-le chez le vétérinaire dès que possible.**`
+
+
+const name = 'Prendre soin des dents des chiens et prévenir les maladies'
+const slug = 'prendre-soin-des-dents-des-chiens'
+const exerpt = "Plus de 80% des chiens ont des maladies dentaires avant l'âge de 4 ans. Découvre ici les bases pour prendre soin des dents des chiens pour prévenir ces maladies"
+const minRead = 10
+const content = text
+const metaKeywords = 'soin dentaire chien,sante dentaire chien,sante chien,hygiene dentaire chien,sante dents chiens,maladies dentaires chien'
+const tags = ['sante-chien']
+
+const post = {
+  name,
+  slug,
+  exerpt,
+  image,
+  minRead,
+  content,
+  metaKeywords,
+  tags
+}
+
+export default post
