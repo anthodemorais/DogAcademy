078e422b72ffd92a506e0cdc27d312bce67b066e
Anthony <anthonyd-m@live.com>
2021-05-26 09:57:19 +0200

    underground fence article

---
+++ src/posts/en/underground-or-buried-wire-fence-for-dog-containment.js
@@ -0,0 +1,54 @@
+import image from '../images/invisible-dog-containment.jpg'
+
+const text = `I remember when I grew up we lived in the country and **everyone** in the neighborhood owned a dog. The streets had little traffic and there was lots of space between the houses and lots of woods and fields for all to roam. You could expect to see any of your friend's dogs during the course of your travels during the day.  It seemed more like the dogs belonged to the neighborhood, but each dog knew where to go for their dinner and when it was time to sleep.  When I turned 16 and began driving, I remember seeing my dog a mile or so away from our house and pulling over in the car to give her a ride home.  From the look on her face I could tell she had a great day exploring the woods, chasing scents, and probably getting into a bit of mischief.  She also looked glad to see me and exhausted. I knew she appreciated the lift.
+
+I look back on the days with fond memories.  But, those days are over.  The reality of today is that the **streets are busier** and more and more people are overly possessive about their 'space' which doesn't allow much sympathy for the roaming dog that knows no boundaries and doesn't understand leash laws or the concept of 'pooper scoopers'.  Heck, neighbors barely know each other anymore let alone their neighbor's dogs.  And, finally, thanks to irresponsible pet owners and our ever-evolving litigious society, we have to fear lawsuits associated with pet ownership.  So, it is now understood (and legislated in most cases) that dog owners must have a reliable way to **contain their dog** within their own yard.  I have to admit this is not all bad despite my rosy colored memories of my youth.
+
+There are many options available to achieve this containment goal.  The obvious include **traditional fencing** or just keeping your dog on a leash every time he is outside.  Some people choose outdoor pens or tying the dog to a cable.  There are benefits and drawbacks to each of these methods.  Finally, a method that is relatively new on the scene uses a **radio frequency** to establish a boundary for your dog (a.k.a. underground fences or invisible fencing).
+
+### Let's take a quick look at some of the benefits and drawbacks to these methods.
+
+**Traditional fencing** - Can be very effective if your dog is not prone to climbing or digging, but it becomes ineffective if a gate is left open by mistake.  This is also a fairly **expensive** containment option and is usually limited to only the backyard.
+
+**Outdoor Pens and Cable tethering** - This is very **restrictive** to the dog and the cable option has the potential for harming the dog if he gets tangled.  In most cases that I've seen the dog stays in one spot in both of these situations, even when the pen is fairly large or the cable is long.  Extensive use of these methods can cause stress to the dog that often translates to a variety of behavior issues.
+
+**Underground Pet Fencing** - This option uses fairly basic technology to help you train your dog where his boundary is and provides an influential reason for him to choose to stay in his own yard.
+
+Since everyone understands how the traditional methods work, I wanted to elaborate on the newest method and explain how it works while addressing some common questions.
+
+### Why an Underground Fence?
+
+There are many reasons to choose this option for containing your pet.  Most obvious is that in some areas of the country traditional fences are **not allowed** because a decision was made to keep an open look to the area.  But, even in areas that allow chain link or other traditional fences, an underground fence can still be a necessary or desired choice for the dog owner.  Dogs contained with this method will not be able to dig or jump through it, will have access to the front yard, and will stay contained **even when a gate is open**.  
+
+The way this technology works is that a radio transmitter is installed inside the house - but on an outside wall.  This transmitter is connected to the wire that is typically buried a few inches under the ground.  This wire simply acts as the antenna for the radio transmitter and broadcasts a radio frequency in the vicinity of the buried wire (antenna).  The dog is wearing a collar (think of it as the radio) that is tuned in to the right 'station' and it picks up the signal when it is close enough to the buried wire.  When the collar picks up the signal it gives a warning tone and then an electric stimulus if the dog doesn't choose to move away from the wire.  With proper training, the dog **quickly learns** his boundaries and decides that he doesn't want to receive the electric stimulus.  It's that simple.  Naturally, with any electronic device, there are a myriad of options and features that can be had, but the basic concept remains the same.  The dog gets a negative stimulus if he carries his 'radio' too close to the 'antenna'. 
+
+There are several questions that people ask when they learn about this technology.  The first is, does it really work?  The answer is, **absolutely**.  Most companies report success in over 99% of the cases.
+
+The next question is, is it **cruel**?  Well, this can easily be debated at length, but in my opinion the answer is a resounding 'no'.  Not because the negative stimulus isn't unpleasant, because it is (if it wasn't it wouldn't work!).  What most people don't know is that a properly trained dog may only receive this stimulus a **small handful of times** in his entire life (most during the initial week of training).  In exchange for this handful of negative experiences your dog will obediently stay in his own yard and avoid the possibility of truly serious injury or possible death from getting hit by a car.  In addition, your dog will avoid spending endless hours at the end of a cable or chain or prison-like confinement in a pen.
+
+The next question is why would I need this if I already have a **real fence**?  The answer to this is that you may not, but you may wish to for several reasons.  You may not need this option if your dog stays in the yard and probably would not run off if a gate were left open.  But, many dogs only see a traditional fence as a **puzzle to solve**.  They either figure out a way to dig or chew through a traditional fence.  I owned a dog years ago that was able to pull apart a chain link fence and break the welds of welded wire fencing.  This dog did incredible things when a storm was approaching.  This same dog even learned to vertically climb many fences.  Even if a traditional fence successfully contains your dog, the owner may choose to add an underground fence in order to take advantage of some of their benefits.  An underground fence can add access to the front yard for your dog and will prevent your dog from running through an open gate.
+
+There are many things to consider when deciding to own a pet, and one of the most important is how you will be able to contain him safely in your yard.  It is no longer a choice just to open the door and let him run.  Responsible pet ownership requires more today than in the past, as it should.
+`
+
+
+const name = 'Invisible Dog Containment: Underground or Buried Wire Fences'
+const slug = 'underground-or-buried-wire-fence-for-dog-containment'
+const exerpt = "An exploration of the evolution of dog ownership and methods of keeping the dog safe in the yard. A focus on under the ground or invisible containment systems."
+const minRead = 8
+const content = text
+const metaKeywords = 'dog containment,dog fence,underground wire fence,buried wire fence,puppy containment,dog,puppy'
+const tags = ['dog-training']
+
+const post = {
+  name,
+  slug,
+  exerpt,
+  image,
+  minRead,
+  content,
+  metaKeywords,
+  tags
+}
+
+export default post
---
+++ src/posts/fr/clotures-souterraines-pour-chien.js
@@ -0,0 +1,54 @@
+import image from '../images/invisible-dog-containment.jpg'
+
+const text = `Je me souviens que lorsque j'ai grandi, nous vivions à la campagne et **tout le monde** dans le quartier possédait un chien. Les rues étaient peu fréquentées, il y avait beaucoup d'espace entre les maisons et beaucoup de bois et de champs où tout le monde pouvait se promener. Vous pouviez vous attendre à voir tous les chiens de vos amis au cours de vos déplacements pendant la journée.  On avait plutôt l'impression que les chiens appartenaient au quartier, mais chaque chien savait où aller pour son dîner et quand il était temps de dormir.  Lorsque j'ai eu 16 ans et que j'ai commencé à conduire, je me souviens avoir vu mon chien à environ un kilomètre de notre maison et m'être arrêtée dans la voiture pour la raccompagner.  D'après l'expression de son visage, je pouvais dire qu'elle avait passé une excellente journée à explorer les bois, à rechercher des odeurs et à faire quelques bêtises.  Elle semblait également heureuse de me voir et épuisée. Je savais qu'elle avait apprécié le transport.
+
+Je repense à ces jours avec de bons souvenirs.  Mais cette époque est révolue.  La réalité d'aujourd'hui est que les **rues sont plus fréquentées** et que de plus en plus de gens sont excessivement possessifs quant à leur "espace", ce qui ne permet pas beaucoup de sympathie pour le chien errant qui ne connaît pas de limites et ne comprend pas les lois sur les laisses ou le concept de "ramasseur de crottes".  Les voisins se connaissent à peine, et encore moins les chiens de leurs voisins.  Enfin, grâce à des propriétaires d'animaux irresponsables et à notre société litigieuse en constante évolution, nous devons craindre les poursuites judiciaires associées à la possession d'un animal de compagnie.  Ainsi, il est maintenant entendu (et légiféré dans la plupart des cas) que les propriétaires de chiens doivent avoir un moyen fiable de **contenir leur chien** dans leur propre cour.  Je dois admettre que ce n'est pas si mal, malgré mes souvenirs de jeunesse.
+
+Il existe de nombreuses options pour atteindre cet objectif de confinement.  Les plus évidentes sont les clôtures traditionnelles ou le fait de tenir votre chien en laisse chaque fois qu'il est dehors.  Certaines personnes choisissent des enclos extérieurs ou attachent le chien à un câble.  Chacune de ces méthodes présente des avantages et des inconvénients.  Enfin, une méthode relativement nouvelle utilise une **fréquence radio** pour établir une limite pour votre chien (aussi appelée clôture souterraine ou clôture invisible).
+
+### Examinons rapidement les avantages et les inconvénients de ces méthodes.
+
+**Clôtures traditionnelles** - Elles peuvent être très efficaces si votre chien n'a pas tendance à grimper ou à creuser, mais elles deviennent inefficaces si un portail est laissé ouvert par erreur.  Il s'agit également d'une option de confinement assez **coûteuse** et elle est généralement limitée à l'arrière-cour.
+
+**Enclos extérieur et câble d'attache** - Cette option est très **restrictive** pour le chien et le câble peut potentiellement blesser le chien s'il s'emmêle.  Dans la plupart des cas que j'ai vus, le chien reste au même endroit dans ces deux situations, même si l'enclos est assez grand ou si le câble est long.  L'utilisation intensive de ces méthodes peut causer un stress au chien qui se traduit souvent par une variété de problèmes de comportement.
+
+**Clôture souterraine pour animaux de compagnie** - Cette option utilise une technologie assez basique pour vous aider à apprendre à votre chien où se trouve sa limite et lui donner une raison valable de choisir de rester dans sa propre cour.
+
+Puisque tout le monde comprend le fonctionnement des méthodes traditionnelles, j'ai voulu m'étendre sur la méthode la plus récente et expliquer son fonctionnement tout en répondant à certaines questions courantes.
+
+### Pourquoi une clôture souterraine ?
+
+Il existe de nombreuses raisons de choisir cette option pour contenir votre animal.  La plus évidente est que dans certaines régions du pays, les clôtures traditionnelles sont **non autorisées** parce qu'il a été décidé de garder une apparence ouverte à la région.  Mais, même dans les régions qui autorisent les clôtures à mailles losangées ou autres clôtures traditionnelles, une clôture souterraine peut être un choix nécessaire ou souhaité par le propriétaire du chien.  Les chiens enfermés avec cette méthode ne pourront pas creuser ou sauter à travers, auront accès à la cour avant et resteront enfermés **même lorsqu'un portail est ouvert**.  
+
+Cette technologie fonctionne de la manière suivante : un émetteur radio est installé à l'intérieur de la maison, mais sur un mur extérieur.  Cet émetteur est relié à un fil qui est généralement enterré à quelques centimètres sous le sol.  Ce fil fait simplement office d'antenne pour l'émetteur radio et diffuse une fréquence radio à proximité du fil enterré (antenne).  Le chien porte un collier (pensez-y comme à la radio) qui est réglé sur la bonne "station" et capte le signal lorsqu'il est suffisamment proche du fil enterré.  Lorsque le collier capte le signal, il émet un avertissement sonore, puis un stimulus électrique si le chien ne choisit pas de s'éloigner du fil.  Avec une formation appropriée, le chien apprend rapidement ses limites et décide qu'il ne veut pas recevoir le stimulus électrique.  C'est aussi simple que cela.  Naturellement, avec tout appareil électronique, il existe une myriade d'options et de fonctions, mais le concept de base reste le même.  Le chien reçoit un stimulus négatif s'il porte sa "radio" trop près de l'"antenne". 
+
+Les gens se posent plusieurs questions lorsqu'ils apprennent l'existence de cette technologie.  La première est : est-ce que ça marche vraiment ?  La réponse est : **absolument**.  La plupart des entreprises font état de succès dans plus de 99 % des cas.
+
+La question suivante est : est-ce **cruel** ?  Eh bien, on peut facilement en débattre longuement, mais à mon avis, la réponse est un "non" catégorique.  Ce n'est pas parce que le stimulus négatif n'est pas désagréable, car il l'est (s'il ne l'était pas, cela ne marcherait pas !).  Ce que la plupart des gens ne savent pas, c'est qu'un chien correctement dressé ne reçoit ce stimulus qu'une **petite poignée de fois** dans toute sa vie (la plupart pendant la première semaine de dressage).  En échange de cette poignée d'expériences négatives, votre chien restera docilement dans son propre jardin et évitera la possibilité de se blesser gravement ou de mourir en se faisant renverser par une voiture.  En outre, votre chien évitera de passer des heures interminables au bout d'un câble ou d'une chaîne ou d'être enfermé dans un enclos.
+
+La question suivante est : pourquoi en aurais-je besoin si j'ai déjà une **vraie clôture** ?  La réponse à cette question est que vous n'en avez peut-être pas besoin, mais que vous pouvez le souhaiter pour plusieurs raisons.  Vous n'avez peut-être pas besoin de cette option si votre chien reste dans la cour et ne s'enfuirait probablement pas si une barrière était laissée ouverte.  Mais, de nombreux chiens ne voient dans une clôture traditionnelle qu'une **énigme à résoudre**.  Ils trouvent un moyen de creuser ou de ronger une clôture traditionnelle.  J'ai possédé un chien il y a quelques années qui était capable d'arracher un grillage à mailles losangées et de briser les soudures d'une clôture en fil de fer soudé.  Ce chien faisait des choses incroyables à l'approche d'un orage.  Ce même chien a même appris à escalader verticalement de nombreuses clôtures.  Même si une clôture traditionnelle réussit à contenir votre chien, le propriétaire peut choisir d'ajouter une clôture souterraine afin de profiter de certains de ses avantages.  Une clôture souterraine peut ajouter un accès à la cour avant pour votre chien et l'empêchera de courir par une porte ouverte.
+
+Il y a beaucoup de choses à prendre en compte lorsque vous décidez de posséder un animal de compagnie, et l'une des plus importantes est de savoir comment vous allez pouvoir le contenir en toute sécurité dans votre cour.  Il n'est plus possible de se contenter d'ouvrir la porte et de le laisser courir.  La possession responsable d'un animal de compagnie exige davantage aujourd'hui que par le passé, comme il se doit.
+`
+
+
+const name = 'Clôtures invisibles pour chiens : Les clôtures souterraines'
+const slug = 'clotures-souterraines-pour-chien'
+const exerpt = "L'évolution des méthodes pour assurer la sécurité du chien dans le jardin ou dans la maison. Tu découvriras les systèmes de clôtures souterrains et invisibles."
+const minRead = 8
+const content = text
+const metaKeywords = 'clotures chien,barrieres chien,clotures souterraines,clotures enterrees,clotures chiots,chien,chiot'
+const tags = ['dresser-son-chien']
+
+const post = {
+  name,
+  slug,
+  exerpt,
+  image,
+  minRead,
+  content,
+  metaKeywords,
+  tags
+}
+
+export default post
